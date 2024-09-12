16377528226b2ab789dfb84f55db11ea82a3b2b2
HasnainH311 <19hhaider@heckgrammar.co.uk>
2024-09-12 09:29:30 +0100

    Add files via upload

---
+++ concatenationTask complete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Sep_concatenationTask
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string firstName, lastName;
+            int age;
+            Console.Write("Enter first name: ");
+            firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            lastName = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello " + firstName + " " + lastName + " you are " + age + " years old");
+            Console.ReadLine();
+        }
+    }
+}
